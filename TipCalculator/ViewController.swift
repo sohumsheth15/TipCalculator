65db389b81ac9cf19074e9bbb54215f2e0389c8c
Sohum Sheth <sohumsheth15@gmail.com>
2020-04-10 20:45:02 -0700

    Initial Commit

---
+++ TipCalculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TipCalculator
+//
+//  Created by Sohum Sheth on 4/10/20.
+//  Copyright © 2020 Sohum Sheth. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
